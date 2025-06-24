f1158fda26cd866ecc9fdf2985ab3e9d29b7f5be
George Vasiliadis <gvasiliadis72@gmail.com>
2025-06-24 12:34:10 +1000

    Text box added

--- Country_Guessing_Game_GUI/Game_GUI.py
+++ Country_Guessing_Game_GUI/Game_GUI.py
@@ -2,6 +2,10 @@ import tkinter as tk
 
 root = tk.Tk()
 
+e = tk.Entry(root, width=50)
+e.grid(row=4, column=0)
+e.insert(0, "Enter Your Name...")
+
 # Creating a label widget
 myLabel1 = tk.Label(root, text="Country Guessing Game")
 myLabel2 = tk.Label(root, text="Welcome To The Country Guessing Game!")
@@ -10,10 +14,11 @@ myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
 
 def myClick():
-    myLabel = tk.Label(root, text="Let's Begin!")
+    hello = "Let's Begin "+ e.get() + "!"
+    myLabel = tk.Label(root, text=hello)
     myLabel.grid(row=3, column=0)
 
-myButton = tk.Button(root, text="Press To Start", padx=50, pady=10, command=myClick, fg="blue", bg="#000000")
+myButton = tk.Button(root, text="Press To Start", padx=50, pady=10, command=myClick)
 myButton.grid(row=2, column=0)
 
 root.mainloop()
