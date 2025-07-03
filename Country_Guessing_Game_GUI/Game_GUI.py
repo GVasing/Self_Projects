dc36c35b4e996a8aeaacbea9b0d5b58cd043eeb3
George Vasiliadis <gvasiliadis72@gmail.com>
2025-07-03 13:04:52 +1000

    Player name textbox variable renamed to better reflect purpose.

--- Country_Guessing_Game_GUI/Game_GUI.py
+++ Country_Guessing_Game_GUI/Game_GUI.py
@@ -34,18 +34,31 @@ myLabel1 = tk.Label(root, text="Welcome To The Country Guessing Game!")
 # Shoving it onto the screen
 myLabel1.grid(row=0, column=0, pady=5)
 
-# Output text box
-e = tk.Entry(root, width=50)
-e.grid(row=4, column=0, padx=5, pady=5)
-e.insert(0, "Enter Your Name...")
+# Output text box for player name
+player_name = tk.Entry(root, width=50)
+player_name.grid(row=4, column=0, padx=5, pady=5)
+player_name.insert(0, "Enter Your Name...")
 
 # Function for Button created/defined
 def myClick():
+    # Print 'hello' with player name
     hello = "Let's Begin "+ e.get() + "!"
     myLabel = tk.Label(root, text=hello)
-    # if True:
-    #     myLabel.destroy()
     myLabel.grid(row=3, column=0)
+
+    # # Connect to database
+    # conn = sqlite3.connect("player_data.db")
+
+    # # Create/Define cursor
+    # c = conn.cursor()
+
+    # # Insert data into table
+    # c.execute("INSERT INTO players VALUES (:user_id, :user_name)",
+    #           {
+    #               "user_id"
+    #           }
+              
+    #           )    
 
 # Button inserted/displayed 
 myButton = tk.Button(root, text="Press To Start", padx=50, pady=10, command=myClick)
