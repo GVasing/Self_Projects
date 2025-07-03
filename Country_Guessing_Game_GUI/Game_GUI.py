219da6826cab8f4b95b8404a9a48633807de76f9
George Vasiliadis <gvasiliadis72@gmail.com>
2025-07-03 14:51:06 +1000

    Function for change entry box text colour when being typed added.

--- Country_Guessing_Game_GUI/Game_GUI.py
+++ Country_Guessing_Game_GUI/Game_GUI.py
@@ -30,7 +30,34 @@ conn.commit()
 # Close connection
 conn.close()
 
-# Function for Button created/defined
+# Entry textbox colour change function
+def text_colour_change(event):
+    current_text=player_name.get()
+    if current_text.lower():
+        player_name.config(fg="black")
+    else:
+        player_name.config(fg="grey")
+
+def game_message():    
+
+    # Define variable for opening message text
+    opening_message = """Let's quickly go over the rules of the game.
+A country will be randomly selected, and you will have to guess which country it is.
+With the help of hints, you'll be able to narrow down the possibilities.
+If you're a beginner, start with EASY mode, where the hints are plenty, and the guesses are endless.
+If you're a bit more confident, play INTERMEDIATE.  Here you'll be given plenty of hints, but the guesses are limited to 10, so choose wisely.
+If you think you're a pro, play HARD. It'll be the same format, but hints will limited and obscure, while guesses will be capped at 5.
+OR, if you're 'FEELING LUCKY', try and guess the Country in one, with no hint(s).
+Press Enter when you're ready(Or 'Quit' if you're not ready). Best of luck!.
+""" 
+    # Create/Define label for message
+    opening_message_label = Label(root, text=opening_message)
+
+    # Display label(message)
+    opening_message_label.grid(row=4, column=0)
+
+
+# Function for Player start button created/defined
 def myClick():
     # Print 'hello' with player name
     hello = "Let's Begin "+ player_name.get() + "!"
@@ -58,6 +85,9 @@ def myClick():
     # Clear textbox
     player_name.delete(0, END)
 
+    # Run opening message function
+    game_message()
+
 # Positioning onto the screen
 myLabel1.grid(row=0, column=0, pady=5)
 
@@ -65,7 +95,6 @@ myLabel1.grid(row=0, column=0, pady=5)
 player_name = Entry(root, width=50, fg="grey")
 player_name.grid(row=4, column=0, padx=5, pady=5)
 player_name.insert(0, "Enter Your Name...")
-player_name.config(fg="black")
 
 # Button inserted/displayed 
 myButton = Button(root, text="Press To Start", padx=50, pady=10, command=myClick)
@@ -77,6 +106,9 @@ my_label = Label(image=my_img)
 
 # Background image displayed
 my_label.grid(row=5, column=0)
+
+# Bind text colour change function
+player_name.bind("<KeyRelease>", text_colour_change)
 
 # Run code/Open window
 root.mainloop()
--- Country_Guessing_Game_GUI/test.py
+++ Country_Guessing_Game_GUI/test.py
@@ -1,4 +1,5 @@
-# import sqlite3
+import sqlite3
+from tkinter import *
 
 # conn = sqlite3.connect("player_data.db")
 
@@ -6,3 +7,41 @@
 
 # c.execute("SELECT rowid, * FROM players")
 # print(c.fetchall())
+
+root = Tk()
+root.geometry("400x400")
+
+# user_name = "George"
+
+# opening_message = (f"""
+# Welcome {user_name}
+# Let's quickly go over the rules of the game.
+# A country will be randomly selected, and you will have to guess which country it is.
+# With the help of hints, you'll be able to narrow down the possibilities.
+# If you're a beginner, start with EASY mode, where the hints are plenty, and the guesses are endless.
+# If you're a bit more confident, play INTERMEDIATE.  Here you'll be given plenty of hints, but the guesses are limited to 10, so choose wisely.
+# If you think you're a pro, play HARD. It'll be the same format, but hints will limited and obscure, while guesses will be capped at 5.
+# OR, if you're 'FEELING LUCKY', try and guess the Country in one, with no hint(s).
+# Press Enter when you're ready(Or 'Quit' if you're not ready). Best of luck!.
+# """)
+
+# label = Label(root, text=opening_message)
+# label.pack()
+
+# def text_colour_change(event):
+#     current_text=player_name.get()
+#     if current_text.lower():
+#         player_name.config(fg="black")
+#     else:
+#         player_name.config(fg="grey")
+
+# Output text box for player name
+# player_name = Entry(root, width=50, fg="grey")
+# player_name.grid(row=4, column=0, padx=5, pady=5)
+# text = Text(root)
+# player_name.insert(0, "Enter Your Name...")
+
+# player_name.bind("<KeyRelease>", text_colour_change)
+
+root.mainloop()
+
